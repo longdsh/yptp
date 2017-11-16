22e58d8432b7b121b7dc0cb13bea5e36af7e9686
longdsh <212977541@qq.com>
2017-11-16 14:12:17 +0800

    1

---
+++ yptp.sql
@@ -0,0 +1,62 @@
+/*
+SQLyog Ultimate v10.00 Beta1
+MySQL - 5.1.37-community : Database - yptp
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`yptp` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `yptp`;
+
+/*Table structure for table `prize` */
+
+DROP TABLE IF EXISTS `prize`;
+
+CREATE TABLE `prize` (
+  `name` varchar(20) DEFAULT NULL,
+  `price` int(11) DEFAULT NULL,
+  `number` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `prize` */
+
+/*Table structure for table `teacher` */
+
+DROP TABLE IF EXISTS `teacher`;
+
+CREATE TABLE `teacher` (
+  `id` varchar(20) NOT NULL,
+  `name` varchar(20) DEFAULT NULL,
+  `money` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `teacher` */
+
+insert  into `teacher`(`id`,`name`,`money`) values ('0413654','刘备',500);
+
+/*Table structure for table `teacher_prize` */
+
+DROP TABLE IF EXISTS `teacher_prize`;
+
+CREATE TABLE `teacher_prize` (
+  `tacherId` varchar(20) DEFAULT NULL,
+  `teacherNmae` varchar(20) DEFAULT NULL,
+  `prizeName` varchar(20) DEFAULT NULL,
+  `number` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `teacher_prize` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
